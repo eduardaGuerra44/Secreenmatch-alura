4904b0af88f22a3223e806e80c231d1f7dcf5472
Eduarda Guerra <maria.guerra@bv.com.br>
2023-10-20 14:37:04 -0300

    inicializando consumo de api

---
+++ src/br/com/alura/principal/PrincipalComBusca.java
@@ -0,0 +1,20 @@
+package br.com.alura.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=barbie&apikey=c103065f"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
